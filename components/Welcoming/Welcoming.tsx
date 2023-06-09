bd4c741e1ee5097263dca0d0245a6f6c806c3f5b
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-06-09 12:15:31 +0300

    ...

--- components/Welcoming/Welcoming.tsx
+++ components/Welcoming/Welcoming.tsx
@@ -1,12 +1,10 @@
-'use client';
-
 import React from 'react';
 import Link from 'next/link';
 import style from './Welcoming.module.css';
 
 import { welcomeWording } from './welcoming.wording';
 
-// import { useAppStore } from '@nhr/store';
+import { DarkLightButton } from '@nhr/components';
 
 export type WelcomingProps = {
   className?: string;
@@ -18,6 +16,7 @@ export type WelcomingProps = {
 const Welcoming: React.FC<WelcomingProps> = ({ className }) => {
   return (
     <div className={`${style.welcoming} ${className || ''}`}>
+      <DarkLightButton />
       <h1 className="text-blue-500">{welcomeWording.title}</h1>
       <p>{welcomeWording.subtitle}</p>
       <p>{welcomeWording.content}</p>
@@ -25,7 +24,9 @@ const Welcoming: React.FC<WelcomingProps> = ({ className }) => {
         Source code
       </Link>
       <div>Email account: {welcomeWording.email}</div>
-      <div className="text-blue-800 font-bold">{welcomeWording.author}</div>
+      <div className="text-blue-800 dark:text-yellow-600 font-bold">
+        {welcomeWording.author}
+      </div>
     </div>
   );
 };
