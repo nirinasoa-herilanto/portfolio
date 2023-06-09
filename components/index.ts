098334eef905b5d6e0c28646511bbda0224e313e
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-06-09 18:38:03 +0300

    feat: add new TextGradiant component
    Use to styling text with an special gradiant effect

--- components/index.ts
+++ components/index.ts
@@ -5,5 +5,11 @@ export { default as Button } from './UI/Button/Button';
 export type { DarkLightButtonProps } from './UI/DarkLightButton/DarkLightButton';
 export { default as DarkLightButton } from './UI/DarkLightButton/DarkLightButton';
 
+export type { TextGradiantProps } from './UI/TextGradiant/TextGradiant';
+export { default as TextGradiant } from './UI/TextGradiant/TextGradiant';
+
+// ----
 export { default as Welcoming } from './Welcoming/Welcoming';
+
+// Theme providers
 export { default as ThemeWrapper } from './ThemeWrapper';
