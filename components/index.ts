3a0ab54c3f012b61f5b82998153e23b6d4697a3b
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-06-10 18:59:30 +0300

    update reexport paths components

--- components/index.ts
+++ components/index.ts
@@ -8,6 +8,13 @@ export { default as DarkLightButton } from './UI/DarkLightButton/DarkLightButton
 export type { TextGradiantProps } from './UI/TextGradiant/TextGradiant';
 export { default as TextGradiant } from './UI/TextGradiant/TextGradiant';
 
+export type { CardProps } from './UI/Card/Card';
+export { default as Card } from './UI/Card/Card';
+
+export type { ProgressBarProps } from './UI/ProgressBar/ProgressBar';
+export { default as ProgressBar } from './UI/ProgressBar/ProgressBar';
+
+export { default as LoadingSpinner } from './UI/LoadingSpinner/LoadingSpinner';
 // ----
 export { default as Welcoming } from './Welcoming/Welcoming';
 
