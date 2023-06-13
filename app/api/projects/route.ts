4ea189115904220298a6964127684599a0e1fdf9
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-06-13 10:51:12 +0300

    update: implementation pagination data with custom response
    by default data will be paginated by 6

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,9 +1,30 @@
 import { NextRequest, NextResponse } from 'next/server';
 import data from './data.json';
-import { IProject } from '@nhr/utils';
+
+import { ICustomResponse, IProject } from '@nhr/utils';
+
+import { appConfig } from '@nhr/config';
 
 export async function GET(request: NextRequest) {
-  const projects = NextResponse.json<IProject[]>(data);
+  const { searchParams } = new URL(request.url);
+  const page = searchParams.get('page');
+
+  let projects = NextResponse.json<ICustomResponse<IProject[]>>({
+    data: data.slice(0, +appConfig.paginationPerPage),
+    results: data.length,
+  });
+
+  if (page) {
+    const start = (+page - 1) * +appConfig.paginationPerPage;
+    const end = +page * +appConfig.paginationPerPage;
+
+    const paginatedData = data.slice(start, end);
+
+    projects = NextResponse.json<ICustomResponse<IProject[]>>({
+      data: paginatedData,
+      results: data.length,
+    });
+  }
 
   return projects;
 }
