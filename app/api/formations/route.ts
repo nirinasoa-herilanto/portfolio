46fb55a7e935bfb2ad53ceda34bcbf8430524ff0
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-06-13 10:49:52 +0300

    update: custom response data

--- app/api/formations/route.ts
+++ app/api/formations/route.ts
@@ -1,10 +1,13 @@
 import { NextRequest, NextResponse } from 'next/server';
 import data from './data.json';
 
-import { IFormation } from '@nhr/utils';
+import { ICustomResponse, IFormation } from '@nhr/utils';
 
 export async function GET(request: NextRequest) {
-  const formations = NextResponse.json<IFormation[]>(data);
+  const formations = NextResponse.json<ICustomResponse<IFormation[]>>({
+    data,
+    results: data.length,
+  });
 
   return formations;
 }
