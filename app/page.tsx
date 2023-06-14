84cc654cde605540e6b1f93f71f7e5438147e0b5
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-06-14 19:11:49 +0300

    update: test data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
-import { Hero } from '@nhr/components';
+import { fetchApi } from '@nhr/services';
 
-export default function Homepage() {
+import { appConfig } from '@nhr/config';
+
+import { ICustomResponse, IProject, ISkill } from '@nhr/utils';
+
+import { Hero, ProjectList, SkillList } from '@nhr/components';
+
+export default async function Homepage() {
+  const { data } = await fetchApi<ICustomResponse<ISkill[]>>(
+    `${appConfig.apiEndpoint}/api/skills`
+  );
+
+  const { data: projects } = await fetchApi<ICustomResponse<IProject[]>>(
+    `${appConfig.apiEndpoint}/api/projects`
+  );
+
   return (
-    <section className="homepage flex justify-center items-center">
+    <section className="homepage">
       <Hero />
+
+      <div className="my-skills mb-12">
+        <h1 className="mb-8">My skills</h1>
+        <SkillList data={data} />
+      </div>
+
+      <div className="my-projects mb-12">
+        <h1 className="mb-8">My projects</h1>
+        <ProjectList data={projects} />
+      </div>
     </section>
   );
 }
