f8dcfed36bcffeaab1bf02138226d2b89b24ee57
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-06-20 18:54:22 +0300

    update: about page

---
+++ app/about/about.wording.ts
@@ -0,0 +1,15 @@
+export const aboutWording = {
+  greeting: 'Hello,',
+  pitch: `
+  I am Nirinasoa Herilanto Randriamananjara and you can call me Herilanto.
+  I am a full-stack developer, passionate about programming, and digital transformations.
+  I enjoy researching those topics by providing numerical solutions or solving related
+  issues, such as: building a beautiful web applications, static websites, ... 
+  `,
+  preference: `
+  Personally, I am focused on Javascript language with React JS for building the UI, and I like working
+  MERN stack with Typescript. As a reference, I like Javascript when working with NoSQL data, but
+  I am able to work with SQL data using Flask (a micro framework of Python), SQLAlchemy, and SQLAlchemy ORM.
+  `,
+  profileLink: `https://res.cloudinary.com/nhr/image/upload/v1687273423/profile.jpg`,
+};
